8573251c01dd8e6fb9e4bb3447473d32a40b8409
Ben Appleton <ben@bios.health>
2020-12-02 12:58:40 +0000

    Day 2 part 2

--- Challenges/day2/src/day2.cpp
+++ Challenges/day2/src/day2.cpp
@@ -99,6 +99,76 @@ private:
     }
 };
 
+class NewPasswordEntry {
+
+public: 
+    NewPasswordEntry(std::string const &in) : m_raw(in) {
+
+        // Just in case we need to not be case-sensitive
+        //transform(m_raw.begin(), m_raw.end(), m_raw.begin(), ::tolower);
+
+        size_t d1pos, d2pos, d3pos;
+        d1pos = m_raw.find(m_delim1);
+        d2pos = m_raw.find(m_delim2);
+        d3pos = m_raw.find(m_delim3);
+
+        std::string part1 = m_raw.substr(0, d1pos);
+        std::string part2 = m_raw.substr(d1pos+1, d2pos - d1pos);
+        std::string part3 = m_raw.substr(d2pos+1, 1);
+        std::string part4 = m_raw.substr(d3pos+2, in.npos - d3pos);
+
+        m_pos_1 = std::stoi(part1) - 1;
+        m_pos_2 = std::stoi(part2) - 1;
+        m_char = std::move(part3);
+        m_pass = std::move(part4);
+    }
+
+    void print() {
+        std::cout << "Character: " << m_char << std::endl <<
+                    "Positions: " << m_pos_1 << " or " << m_pos_2 << std::endl <<
+                    "String: " << m_pass << std::endl <<
+                    "Raw: " << m_raw << std::endl;
+        if (m_count_actual != 0) {
+            std::cout << "Actual: " << m_count_actual << std::endl;
+        }
+        std::cout << std::endl;
+
+        
+    }
+
+    bool validate() {
+        return m_pass.at(m_pos_1) == *m_char.c_str() ? (
+            m_pass.at(m_pos_2) == *m_char.c_str() ? false : true
+        ) : (
+            m_pass.at(m_pos_2) == *m_char.c_str() ? true : false
+        );
+    }
+private:
+    size_t m_pos_1;
+    size_t m_pos_2;
+    std::string m_char;
+    std::string m_pass;
+    std::string m_raw;
+
+    const std::string m_delim1 = "-";
+    const std::string m_delim2 = " ";
+    const std::string m_delim3 = ": ";
+
+    size_t count(void) {
+        size_t _count=0;
+        size_t _pos = 0;
+        while (_pos != m_pass.npos) {
+            _pos = m_pass.find(m_char, _pos);
+            if (_pos != m_pass.npos) {
+                _count++;
+                _pos++;
+            }
+        }
+        m_count_actual = _count;
+        return m_count_actual;
+    }
+};
+
 
 
 int main(int argc, char **argv) {
@@ -128,20 +198,35 @@ int main(int argc, char **argv) {
     // Input is XX-YY C: [string]
     // Iterate through the strings and convert them to PasswordEntry
 
+    std::cout << "Validating against old password policy" << std::endl;
+
     size_t pass_count = 0;
     size_t fail_count = 0;
     for (auto it = entries.begin(); it != entries.end(); ++it) {
         PasswordEntry pe(*it);
         if (pe.validate()) {
             pass_count++;
-            std::cout << "Success" << std::endl;
-            pe.print();
         }
         else {
             fail_count++;
         }
     }
+    std::cout << "Done. Found " << pass_count << " passes and " << fail_count << " failures." << std::endl;
 
+    std::cout << "Validating against new password policy" << std::endl;
+
+    pass_count = 0;
+    fail_count = 0;
+
+    for (auto it = entries.begin(); it != entries.end(); ++it) {
+        NewPasswordEntry npe(*it);
+        if (npe.validate()) {
+            pass_count++;
+        }
+        else {
+            fail_count++;
+        }
+    }
     std::cout << "Done. Found " << pass_count << " passes and " << fail_count << " failures." << std::endl;
 
 }
